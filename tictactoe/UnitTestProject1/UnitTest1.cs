50d76c82f14431b2225fe4904d9c8e66b616adcb
yhy4 <tuf61857@temple.edu>
2018-10-23 18:47:46 -0400

    test

---
+++ tictactoe/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tictactoe;
+
+namespace tictactoe.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void CurrentPlayerisXTest()
+        {
+            tictactoe tictactoe = new tictactoe();
+            string currentplayer = tictactoe.CurrentPlayer(4);
+            Assert.AreEqual("X", currentplayer);
+        }
+        [TestMethod()]
+        public void CurrentPlayerisOTest()
+        {
+            tictactoe tictactoe = new tictactoe();
+            string currentplayer = tictactoe.CurrentPlayer(1);
+            Assert.AreEqual("O", currentplayer);
+        }
+
+        [TestMethod()]
+        public void BoardIsNineField()
+        {
+            tictactoe tictactoe = new tictactoe();
+            tictactoe.CreateBoard();
+            Assert.AreEqual(9, tictactoe.TictactoeArray.Length);
+        }
+        [TestMethod()]
+        public void CurrentPlayerPlaceX()
+        {
+            tictactoe tictactoe = new tictactoe();
+            tictactoe.CreateBoard();
+            tictactoe.MakeMove(2, "X");
+            Assert.AreEqual("X", tictactoe.TictactoeArray[2]);
+        }
+        [TestMethod()]
+        public void CurrentPlayerPlaceO()
+        {
+            tictactoe tictactoe = new tictactoe();
+            tictactoe.CreateBoard();
+            tictactoe.MakeMove(1, "O");
+            Assert.AreEqual("O", tictactoe.TictactoeArray[1]);
+        }
+        [TestMethod()]
+        public void placementIsUsed()
+        {
+            tictactoe tictactoe = new tictactoe();
+            tictactoe.CreateBoard();
+            bool inUsed= tictactoe.SpaceinUse(3);
+            Assert.AreEqual(false, inUsed);
+        }
+
+    }
+}
+
---
+++ tictactoe/tictactoe.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tictactoe
+{
+    public class tictactoe
+    {
+        public string currentplayer;
+        public string[] TictactoeArray;
+        public void CreateBoard() {
+             TictactoeArray = new string [9];
+        }
+        public string CurrentPlayer(int turn) {
+            if (turn % 2 == 0)
+            {
+                currentplayer = "X";
+            }
+            else {
+                currentplayer = "O";
+            }
+            return currentplayer;
+        }
+        public void ChangePlayer() {
+            if (currentplayer == "X")
+            {
+                currentplayer = "O";
+            }
+            else {
+                currentplayer = "X";
+            }
+        }
+        public void MakeMove(int position, string currentplayer) {
+            
+             TictactoeArray[position]=currentplayer;
+        }
+        public bool SpaceinUse(int position) {
+            if (TictactoeArray[position] != "")
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+    }
+
+
